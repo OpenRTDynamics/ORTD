3b7be1afa7c7872d08e08739157f108a11c6573f
christianausb <christianausb@b1787af3-3966-47a0-bf5d-3e5e26f13500>
2011-11-11 15:59:20 +0000

    scope readout client

--- trunk/modules/rt_server/ortd_buffered_io.h
+++ trunk/modules/rt_server/ortd_buffered_io.h
@@ -5,6 +5,22 @@
 #define _ORTD_BUFFERED_IO_H 1
 
 
+// class ortd_buffered_io {
+// public:
+//   ortd_buffered_io(int fd);
+//   ~ortd_buffered_io();
+//   
+//   int waitforaline();
+//   int writeln(char *line);
+//   
+//   std::stringstream sstream_in;
+// 
+// private:
+//   int fd;
+//   
+//   
+// };
+
 class ortd_buffered_io {
 public:
   ortd_buffered_io(int fd);
--- trunk/modules/rt_server/remote_decoder.h
+++ trunk/modules/rt_server/remote_decoder.h
@@ -3,36 +3,59 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <map>
+#include <sstream>
+#include "ortd_buffered_io.h"
 
 class ortd_tcpclient;
 class rt_server_decoder;
 
-class rt_server_decoder_scope {
-  public:
-    rt_server_decoder_scope (rt_server_decoder* decoder);
-    ~rt_server_decoder_scope ();
-    
-    bool read_single_vec(double *wrtite_to, bool blocking);
-    
-    int veclen;
-    char *name;
-    
-  private:
-    class rt_server_decoder_iohelper * myiohelper;
-    rt_server_decoder* decoder;
-};
+
 
 class ortd_tcpclient {
   private:
     int sd;
+    int fd;
+    
     FILE *buffer_fd;
     
   public:
+    
     ortd_tcpclient();
     ~ortd_tcpclient();
     
     bool tcpconnect(char *hostname, int port);
     
+    int get_fd();
+    
+    void closeconnection();
+    
+};
+
+
+class rt_server_decoder_scope {
+  public:
+    rt_server_decoder_scope (rt_server_decoder* decoder, std::string name);
+    ~rt_server_decoder_scope ();
+    
+    void setInfo(int veclen, int datatype, int StreamId);
+    void start_stream();
+    void stop_stream();
+    
+    bool read_single_vec_blocking(double *wrtite_to);
+    void enqueue_data(double *data);
+    
+    std::string name;
+    
+  private:
+    
+//     std::queue<std::string> dataqueue;
+    
+/*    ortd_tcpclient *io;
+    ortd_buffered_io *bio;*/
+    rt_server_decoder* decoder;
+    int veclen; int datatype; int StreamId;
+    
+    void *databuffer;
 };
 
 class rt_server_decoder {
@@ -43,11 +66,22 @@ class rt_server_decoder {
 //     void init_get_scope();
     rt_server_decoder_scope * get_next_scope(); // Multiple calls return a list of all scopes
 
+    rt_server_decoder_scope * get_scope_by_id(int id);
+    rt_server_decoder_scope * get_scope_by_name(std::string name);
+    
+    bool read_scopes();
+    bool await_message();
     
     char *hostname;
     int port;
+
+    ortd_tcpclient *io;
+    ortd_buffered_io *bio;
     
   private:
-    rt_server_decoder_iohelper *myiohelper;
+    
+    typedef std::map<int, rt_server_decoder_scope *> scope_map;
+    scope_map scopes;
+
     
 };
