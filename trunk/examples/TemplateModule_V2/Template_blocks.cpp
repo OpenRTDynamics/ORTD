5a58c092685fd0a5f4aaa30bb348ab422ced33ea
christianausb <christianausb@b1787af3-3966-47a0-bf5d-3e5e26f13500>
2013-05-14 15:19:10 +0000

    cleaned up

--- trunk/examples/TemplateModule_V2/Template_blocks.cpp
+++ trunk/examples/TemplateModule_V2/Template_blocks.cpp
@@ -32,9 +32,9 @@ extern "C" {
 
 
 
-class compu_func_EDF_class {
+class compu_func_Template_class {
 public:
-    compu_func_EDF_class(struct dynlib_block_t *block);
+    compu_func_Template_class(struct dynlib_block_t *block);
     void destruct();
     void io(int update_states);
     int init();
@@ -45,12 +45,12 @@ private:
 
 };
 
-compu_func_EDF_class::compu_func_EDF_class(dynlib_block_t* block)
+compu_func_Template_class::compu_func_Template_class(dynlib_block_t* block)
 {
     this->block = block;
 }
 
-int compu_func_EDF_class::init()
+int compu_func_Template_class::init()
 {
     // initialise your block
     
@@ -87,7 +87,7 @@ int compu_func_EDF_class::init()
 }
 
 
-void compu_func_EDF_class::io(int update_states)
+void compu_func_Template_class::io(int update_states)
 {
     if (update_states==0) { // calculate outputs
 //         double *in = (double *) libdyn_get_input_ptr(block,0); // the first input port
@@ -98,17 +98,17 @@ void compu_func_EDF_class::io(int update_states)
     }
 }
 
-void compu_func_EDF_class::destruct()
+void compu_func_Template_class::destruct()
 {
     // free your memory, ...
 }
 
 
 // This is the main C-Callback function, which forwards requests to the C++-Class above
-int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
+int compu_func_TemplateWrite(int flag, struct dynlib_block_t *block)
 {
 
-     printf("comp_func EDF: flag==%d\n", flag);
+     printf("comp_func Template: flag==%d\n", flag);
 
     double *rpar = libdyn_get_rpar_ptr(block);
     int *ipar = libdyn_get_ipar_ptr(block);
@@ -119,7 +119,7 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
     case COMPF_FLAG_CALCOUTPUTS:
     {
 //         in = (double *) libdyn_get_input_ptr(block,0);
-        compu_func_EDF_class *worker = (compu_func_EDF_class *) libdyn_get_work_ptr(block);
+        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
 
         worker->io(0);
 
@@ -129,7 +129,7 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
     case COMPF_FLAG_UPDATESTATES:
     {
 //         in = (double *) libdyn_get_input_ptr(block,0);
-        compu_func_EDF_class *worker = (compu_func_EDF_class *) libdyn_get_work_ptr(block);
+        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
 
         worker->io(1);
 
@@ -141,55 +141,12 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
         int i;
 	
 	return libdyn_AutoConfigureBlock(block, ipar, rpar);
-
-	
-// 	// get parameters
-//      struct irpar_ivec_t insizes_irp, outsizes_irp, intypes_irp, outtypes_irp, dfeed_irp;
-// 
-//     int error = 0; // FIXME Länge prüfen
-//     if ( irpar_get_ivec(&insizes_irp, ipar, rpar, 10) < 0 ) error = 1 ;
-//     if ( irpar_get_ivec(&outsizes_irp, ipar, rpar, 11) < 0 ) error = 1 ;
-//     if ( irpar_get_ivec(&intypes_irp, ipar, rpar, 12) < 0 ) error = 1 ;
-//     if ( irpar_get_ivec(&outtypes_irp, ipar, rpar, 13) < 0 ) error = 1 ;
-//     if ( irpar_get_ivec(&dfeed_irp, ipar, rpar, 14) < 0 ) error = 1 ;
-// 
-//      struct irpar_ivec_t Uipar;
-//      struct irpar_rvec_t Urpar;
-//     
-//     if ( irpar_get_ivec(&Uipar, ipar, rpar, 20) < 0 ) error = 1 ;
-//     if ( irpar_get_rvec(&Urpar, ipar, rpar, 21) < 0 ) error = 1 ;
-// 
-// 
-// 
-// 	 // The number of in- and output ports
-//         int Nin = insizes_irp.n;
-//         int Nout = outsizes_irp.n; //ipar[1];
-// 
-// 	
-// 	libdyn_config_block(block, BLOCKTYPE_DYNAMIC, Nout, Nin, (void *) 0, 0);
-// 
-// 	// configure each in and output port i
-//         for (i = 0; i < Nin; ++i) {
-// 	    printf("%d - \n", insizes_irp.v[i] );
-//             libdyn_config_block_input(block, i, insizes_irp.v[i], intypes_irp.v[i]);
-// 	}
-// 
-//         for (i = 0; i < Nout; ++i) {
-// 	    printf("%d - df=%d \n", outsizes_irp.v[i], dfeed_irp.v[i] );
-//             libdyn_config_block_output(block, i, insizes_irp.v[i], outtypes_irp.v[i], dfeed_irp.v[i] );
-// 	}
-// //         for (i = 0; i < Nin; ++i)
-// //             libdyn_config_block_output(block, i, 1, DATATYPE_FLOAT, 1);
-/*
-	    
-	 return error;*/
-
     }
     return 0;
     break;
     case COMPF_FLAG_INIT:  // init
     {
-        compu_func_EDF_class *worker = new compu_func_EDF_class(block);
+        compu_func_Template_class *worker = new compu_func_Template_class(block);
         libdyn_set_work_ptr(block, (void*) worker);
 	
         int ret = worker->init();
@@ -200,7 +157,7 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
     break;
     case COMPF_FLAG_DESTUCTOR: // destroy instance
     {
-        compu_func_EDF_class *worker = (compu_func_EDF_class *) libdyn_get_work_ptr(block);
+        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
 
         worker->destruct();
 	delete worker;
@@ -209,7 +166,7 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
     return 0;
     break;
     case COMPF_FLAG_PRINTINFO:
-        printf("I'm an EDF block\n");
+        printf("I'm an Template block\n");
         return 0;
         break;
 
@@ -219,19 +176,19 @@ int compu_func_EDFWrite(int flag, struct dynlib_block_t *block)
 // Export to C so the libdyn simulator finds this function
 extern "C" {
     // ADJUST HERE: must match to the function name in the end of this file
-    int libdyn_module_EDF_siminit(struct dynlib_simulation_t *sim, int bid_ofs);
+    int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs);
 }
 
 // CHANGE HERE: Adjust this function name to match the name of your module
-int libdyn_module_EDF_siminit(struct dynlib_simulation_t *sim, int bid_ofs)
+int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs)
 {
 
     // Register my blocks to the given simulation
 
-    int blockid = 15800;  // CHANGE HERE: choose a unique id for each block
-    libdyn_compfnlist_add(sim->private_comp_func_list, blockid, LIBDYN_COMPFN_TYPE_LIBDYN, (void*) &compu_func_EDFWrite);
+    int blockid = 999911111;  // CHANGE HERE: choose a unique id for each block
+    libdyn_compfnlist_add(sim->private_comp_func_list, blockid, LIBDYN_COMPFN_TYPE_LIBDYN, (void*) &compu_func_TemplateWrite);
 
-    printf("module EDF initialised\n");
+    printf("module Template initialised\n");
 
 }
 
