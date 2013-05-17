3e9a582c8724e2ef285f71099ff378047fc96ebb
christianausb <christianausb@users.noreply.github.com>
2013-05-17 13:40:01 +0000

    now the nicer template

--- examples/TemplateModule_V2/pluginLoader.cpp
+++ examples/TemplateModule_V2/pluginLoader.cpp
@@ -2,7 +2,7 @@
 
 extern "C" {
 
-  int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs);
+  int libdyn_module_TemplateModule_V2_siminit(struct dynlib_simulation_t *sim, int bid_ofs);
 
   int ortd_plugin_init(struct dynlib_simulation_t *sim)
   {
@@ -16,7 +16,7 @@ extern "C" {
 //     
 //     libdyn_compfnlist_add(sim->private_comp_func_list, blockid_ofs, LIBDYN_COMPFN_TYPE_LIBDYN, &compu_func_sensor_actor);
 
-    libdyn_module_Template_siminit(sim, 0);
+    libdyn_module_TemplateModule_V2_siminit(sim, 0);
   
     return 0;
   }
