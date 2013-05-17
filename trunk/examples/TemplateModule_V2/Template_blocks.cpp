d4f43a01abd27c202b0bb635f19dc412abf201df
christianausb <christianausb@b1787af3-3966-47a0-bf5d-3e5e26f13500>
2013-05-17 13:12:28 +0000

    made the example block more nice looking; howver not to the full extend by now

--- trunk/examples/TemplateModule_V2/Template_blocks.cpp
+++ trunk/examples/TemplateModule_V2/Template_blocks.cpp
@@ -21,7 +21,6 @@
 
 extern "C" {
 #include "libdyn.h"
-#include "libdyn_scicos_macros.h"
 #include "irpar.h"
 }
 
@@ -32,161 +31,208 @@ extern "C" {
 
 
 
+/*
+   A nicer C++ interface for compuational functions
+*/
+
+
+
+
+
+/*
+
+  This is a template for creating a new ORTD Block's computational function in C++
+
+  Do a search of "Template" and replace it with your own name.
+
+*/
+
+
 class compu_func_Template_class {
 public:
-    compu_func_Template_class(struct dynlib_block_t *block);
-    void destruct();
-    void io(int update_states);
-    int init();
-private:
-   struct dynlib_block_t *block;
-   
-   double z0; // a state
-
-};
-
-compu_func_Template_class::compu_func_Template_class(dynlib_block_t* block)
-{
-    this->block = block;
-}
-
-int compu_func_Template_class::init()
-{
-    // initialise your block
-    
-    int *Uipar;
-    double *Urpar;
-        
-    // Get the irpar parameters Uipar, Urpar
-    libdyn_AutoConfigureBlock_GetUirpar(block, &Uipar, &Urpar);
-
-    //
-    // extract some structured sample parameters
-    //
-    int error = 0; 
-
-    //
-    // get a string (not so nice by now)
-    //
-    struct irpar_ivec_t str_;
-    char *str;
-    if ( irpar_get_ivec(&str_, Uipar, Urpar, 12) < 0 ) error = -1 ;    
-    irpar_getstr(&str, str_.v, 0, str_.n);
-
-    printf("str = %s\n", str);
-    
-    free(str); // do not forget to free the memory allocated by irpar_getstr
-    
-    
+    compu_func_Template_class(struct dynlib_block_t *block) {
+        this->block = block;
+    }  
+    ~compu_func_Template_class()
+    {
+        printf("Destructor was called\n");
+        // free your allocated memory, ...
+    }
+  
     //     
-    // get a vector of integers (double vectors are similar, replace ivec with rvec)
-    //    
-    struct irpar_ivec_t vec;   
-    if ( irpar_get_ivec(&vec, Uipar, Urpar, 11) < 0 ) error = -1 ;            
-    printf("vec[0] = %d\n", vec.v[0]);
+    // define states
+    //
     
+    double z0; // a state
 
+    //     
+    // initialise your block
+    //
+    
+    int init() {
+        int *Uipar;
+        double *Urpar;
+
+        // Get the irpar parameters Uipar, Urpar
+        libdyn_AutoConfigureBlock_GetUirpar(block, &Uipar, &Urpar);
+
+        //
+        // extract some structured sample parameters
+        //
+        int error = 0;
+
+        //
+        // get a string (not so nice by now)
+        //
+        struct irpar_ivec_t str_;
+        char *str;
+        if ( irpar_get_ivec(&str_, Uipar, Urpar, 12) < 0 ) error = -1 ;
+        irpar_getstr(&str, str_.v, 0, str_.n);
+
+        printf("str = %s\n", str);
+
+        free(str); // do not forget to free the memory allocated by irpar_getstr
+
+
+        //
+        // get a vector of integers (double vectors are similar, replace ivec with rvec)
+        //
+        struct irpar_ivec_t vec;
+        if ( irpar_get_ivec(&vec, Uipar, Urpar, 11) < 0 ) error = -1 ;
+        printf("vec[0] = %d\n", vec.v[0]);
+
+
+        // set the initial states
+        resetStates();
+
+        // Return -1 to indicate an error, so the simulation will be destructed
+        return error;
+    }
 
     
-    // Return -1 to indicate an error, so the simulation will be destructed  
-    return error;
-}
-
-
-void compu_func_Template_class::io(int update_states)
-{
-  double *in1 = (double *) libdyn_get_input_ptr(block,0); // the first input port
-  double *in2 = (double *) libdyn_get_input_ptr(block,1); // the 2nd input port
-  double *output = (double*) libdyn_get_output_ptr(block, 0); // the first output port
-
-  if (update_states == 1) { // update states
-    z0 = *in1 * 2;
-  } else 
-    if (update_states==0) { // calculate outputs            
-      *output = z0;
-    }
-}
-
-void compu_func_Template_class::destruct()
-{
-    // free your allocated memory, ...
-}
-
-
-// This is the main C-Callback function, which forwards requests to the C++-Class above
-int compu_func_TemplateWrite(int flag, struct dynlib_block_t *block)
-{
-   // uncomment this if you want to know when this block is called by the simulator
-   // printf("comp_func Template: flag==%d\n", flag);
-
-    double *rpar = libdyn_get_rpar_ptr(block);
-    int *ipar = libdyn_get_ipar_ptr(block);
-
-
-
-    switch (flag) {
-    case COMPF_FLAG_CALCOUTPUTS:
+    void updateStates()
     {
-//         in = (double *) libdyn_get_input_ptr(block,0);
-        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+        double *in1 = (double *) libdyn_get_input_ptr(block,0); // the first input port
+        double *in2 = (double *) libdyn_get_input_ptr(block,1); // the 2nd input port
+        double *output = (double*) libdyn_get_output_ptr(block, 0); // the first output port
 
-        worker->io(0);
-
+        z0 = *in1 * 2;
     }
-    return 0;
-    break;
-    case COMPF_FLAG_UPDATESTATES:
+
+    
+    void calcOutputs()
     {
-//         in = (double *) libdyn_get_input_ptr(block,0);
-        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+        double *in1 = (double *) libdyn_get_input_ptr(block,0); // the first input port
+        double *in2 = (double *) libdyn_get_input_ptr(block,1); // the 2nd input port
+        double *output = (double*) libdyn_get_output_ptr(block, 0); // the first output port
 
-        worker->io(1);
-
+//   printf("output calc\n");
+        *output = z0;
     }
-    return 0;
-    break;
-    case COMPF_FLAG_CONFIGURE:  // configure. NOTE: do not reserve memory or open devices. Do this while init instead!
+
+    
+    void resetStates()
     {
-        int i;
-	
-	return libdyn_AutoConfigureBlock(block, ipar, rpar);
+        z0 = 0; // return to the blocks initial state
     }
-    return 0;
-    break;
-    case COMPF_FLAG_INIT:  // init
-    {
-        compu_func_Template_class *worker = new compu_func_Template_class(block);
-        libdyn_set_work_ptr(block, (void*) worker);
-	
-        int ret = worker->init();
-        if (ret < 0)
-            return -1;
-    }
-    return 0;
-    break;
-    case COMPF_FLAG_DESTUCTOR: // destroy instance
-    {
-        compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
 
-        worker->destruct();
-	delete worker;
 
+
+
+    void printInfo() {
+        fprintf(stderr, "I'm a Template block\n");
     }
-    return 0;
-    break;
-    case COMPF_FLAG_PRINTINFO:
-        printf("I'm an Template block\n");
+
+    
+    
+    /*
+       This ugly tail is not so important to look at    
+       Maybe this should be put into a macro
+    */
+
+    // The Computational function that is called by the simulator
+    // and that distributes the execution to the various functions
+    // in this C++ - Class, including: init(), io(), destruct()
+    static int CompFn(int flag, struct dynlib_block_t *block) {
+        // This is the main C-Callback function, which forwards requests to the functions of the C++-Class
+
+        // uncomment this if you want to know when this block is called by the simulator
+//           printf("comp_func Template: flag==%d\n", flag);
+
+        // the blocks raw parameter sets for integers and doubles
+        double *rpar = libdyn_get_rpar_ptr(block);
+        int *ipar = libdyn_get_ipar_ptr(block);
+
+        switch (flag) {
+        case COMPF_FLAG_CALCOUTPUTS:
+        {
+            compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+            worker->calcOutputs();
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_UPDATESTATES:
+        {
+            compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+            worker->updateStates();
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_RESETSTATES:
+        {
+            compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+            worker->resetStates();
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_CONFIGURE:  // configure. NOTE: do not reserve memory or open devices. Do this within COMPF_FLAG_INIT instead!
+        {
+            return libdyn_AutoConfigureBlock(block, ipar, rpar);
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_INIT:  // init
+        {
+            compu_func_Template_class *worker = new compu_func_Template_class(block);
+            libdyn_set_work_ptr(block, (void*) worker); // remember the instance of the C++ Class
+
+            int ret = worker->init();
+            if (ret < 0)
+                return -1;
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_DESTUCTOR: // destroy instance
+        {
+            compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+            delete worker;
+
+        }
+        return 0;
+        break;
+        case COMPF_FLAG_PRINTINFO:
+        {
+            compu_func_Template_class *worker = (compu_func_Template_class *) libdyn_get_work_ptr(block);
+            worker->printInfo();
+        }
         return 0;
         break;
 
-    }
-}
+        }
 
+    }
+    
+    struct dynlib_block_t *block;
+};
+
+
+
+
+//
 // Export to C so the libdyn simulator finds this function
+// All comp. functions for all blocks are added to a list
+//
 extern "C" {
-    // ADJUST HERE: must match to the function name in the end of this file
-    int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs);
-}
 
 // CHANGE HERE: Adjust this function name to match the name of your module
 int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs)
@@ -195,10 +241,13 @@ int libdyn_module_Template_siminit(struct dynlib_simulation_t *sim, int bid_ofs)
     // Register my blocks to the given simulation
 
     int blockid = 999911111;  // CHANGE HERE: choose a unique id for each block
-    libdyn_compfnlist_add(sim->private_comp_func_list, blockid, LIBDYN_COMPFN_TYPE_LIBDYN, (void*) &compu_func_TemplateWrite);
+    
+    libdyn_compfnlist_add(sim->private_comp_func_list, blockid, LIBDYN_COMPFN_TYPE_LIBDYN, (void*) &compu_func_Template_class::CompFn);
+//     libdyn_compfnlist_add(sim->private_comp_func_list, blockid+1, LIBDYN_COMPFN_TYPE_LIBDYN, (void*) &compu_func_Template2_class::CompFn); // another block
 
     printf("module Template initialised\n");
 
 }
 
 
+}
