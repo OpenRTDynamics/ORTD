eb8572c0f7f430719ed89b1430fc23f48970f98f
christianausb <christianausb@b1787af3-3966-47a0-bf5d-3e5e26f13500>
2013-08-07 14:06:07 +0000

    look for more plugins

--- trunk/plugin_loader.c
+++ trunk/plugin_loader.c
@@ -24,9 +24,11 @@ void *ortd_load_plugin(struct dynlib_simulation_t *sim, char *name)
    lib_handle = dlopen(name, RTLD_LAZY);
    if (!lib_handle) 
    {
-      fprintf(stderr, "%s\n", dlerror());
+#ifdef DEBUG
+    fprintf(stderr, "%s\n", dlerror());
+#endif      
 
-     return -1;
+     return NULL;
   }
 
 //    printf("lib loaded\n");
@@ -34,10 +36,15 @@ void *ortd_load_plugin(struct dynlib_simulation_t *sim, char *name)
    init_fn = dlsym(lib_handle, "ortd_plugin_init");
    if ((error = dlerror()) != NULL)  
    {
-      fprintf(stderr, "%s\n", error);
+      // could not open shared lib
+//#ifdef DEBUG
+      fprintf(stderr, "Could not find entry point ortd_plugin_init: %s\n", error);
+      fprintf(stderr, "Make sure you have a C-function called like this.\n");
+      fprintf(stderr, "Maybe you forgot to put this function into   extern \"C\" { .... }.\n");
+//#endif      
       dlclose(lib_handle);
 
-     return -1;
+     return NULL;
   }
 
 // printf("got init fn\n");
@@ -46,12 +53,14 @@ void *ortd_load_plugin(struct dynlib_simulation_t *sim, char *name)
    int ret = (*init_fn)(sim);
 
    if (ret != 0) {
-     fprintf(stderr, "libdyn: Init of plugin %s faild with return values %d\n", name, ret);
+     fprintf(stderr, "libdyn: Init of plugin %s faild with return value %d\n", name, ret);
 //      exit(1);
      dlclose(lib_handle);
-     return -1;
+     return NULL;
    }
 //       printf("called init fn\n");
+
+  fprintf(stderr, "libdyn: Initialisation of plugin %s finished\n", name);
 
    return lib_handle;
 
