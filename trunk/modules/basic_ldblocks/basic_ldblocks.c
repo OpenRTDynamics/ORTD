f5d92a2438be2bd2b4125bd1b1e09ccdb356b7b4
christianausb <christianausb@b1787af3-3966-47a0-bf5d-3e5e26f13500>
2016-10-27 16:11:23 +0000

    Fixed a Bug in ld_steps2 preventing the proper reset of the block

--- trunk/modules/basic_ldblocks/basic_ldblocks.c
+++ trunk/modules/basic_ldblocks/basic_ldblocks.c
@@ -1927,6 +1927,7 @@ int ortd_compu_func_steps2(int flag, struct dynlib_block_t *block)
         break;
     case COMPF_FLAG_RESETSTATES:
         *state = 0;
+	*counter = 0;
 	
         output = (double *) libdyn_get_output_ptr(block,0);
 	*output = *state;
