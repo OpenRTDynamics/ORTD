8f64239ca26e607f2d5e054976ad024a38b610fa
christianausb <christianausb@users.noreply.github.com>
2018-02-19 22:20:08 +0000

    blocks..

--- libdyn.c
+++ libdyn.c
@@ -1,5 +1,5 @@
 /*
-    Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016  Christian Klauer
+    Copyright (C) 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018  Christian Klauer
 
     This file is part of OpenRTDynamics, the Real Time Dynamics Framework
 
